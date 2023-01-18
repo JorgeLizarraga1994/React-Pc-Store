742ebdc81ae98f02ac6d6c1432d3ace62dff9cd2
JorgeLizarraga1994 <jorgelizarraga1994@gmail.com>
2023-01-18 14:49:50 -0300

    cuarto commit

--- e-comerce-jorge-lizarraga/src/components/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-   const ItemsListContainer = () => {
-        return(
-            <h4 className="itemsListContainer">BIENVENIDOS!!!</h4>
-        )
-    }
- export default ItemsListContainer  
